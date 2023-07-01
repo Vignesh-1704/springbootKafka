e09a7d408853cc97f0aa024d3b12f0f2cbdc201d
Vignesh-1704 <vigneshmanikavasagam17@gmail.com>
2023-07-01 16:41:58 +0530

    Add. Added the Initial Configuration for Producer and Consumer

---
+++ src/test/java/com/m2p/springbootKafka/SpringbootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2p.springbootKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
